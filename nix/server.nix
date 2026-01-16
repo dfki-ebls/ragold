d59ad0c2b169414b09cf7fcaba1bbe9d66df101d
Mirko Lenz <mirko@mirkolenz.com>
2026-01-16 11:01:30 +0100

    build: simplify caddy

--- nix/server.nix
+++ nix/server.nix
@@ -1,15 +1,13 @@
 {
   writeShellApplication,
-  writeTextDir,
+  writeText,
   app,
   lib,
-  stdenv,
   caddy,
-  runCommand,
   caddyPort ? 8080,
 }:
 let
-  caddyfileRaw = writeTextDir "Caddyfile" ''
+  caddyfile = writeText "Caddyfile" ''
     {
       admin off
       auto_https off
@@ -20,28 +18,17 @@ let
       root * ${app}
       encode gzip
 
-      # Serve static files with SPA fallback
       handle {
         try_files {path} /index.html
         file_server
       }
     }
   '';
-
-  caddyfileFormatted = runCommand "Caddyfile-formatted" { nativeBuildInputs = [ caddy ]; } /* bash */ ''
-    mkdir -p $out
-    cp --no-preserve=mode ${caddyfileRaw}/Caddyfile $out/Caddyfile
-    caddy fmt --overwrite $out/Caddyfile
-  '';
-
-  caddyfile =
-    if stdenv.buildPlatform == stdenv.hostPlatform then caddyfileFormatted else caddyfileRaw;
 in
 writeShellApplication {
   name = "server";
   text = ''
-    echo "Starting Caddy server on port ${toString caddyPort}..."
-    ${lib.getExe caddy} run --config ${caddyfile}/Caddyfile --adapter caddyfile
+    ${lib.getExe caddy} run --config ${caddyfile} --adapter caddyfile
   '';
   derivationArgs = {
     passthru.port = caddyPort;
