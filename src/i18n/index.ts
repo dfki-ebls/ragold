09ba8829a1535f93cf787e2bc5e6934ef6dc579c
Mirko Lenz <mirko@mirkolenz.com>
2026-01-19 13:29:34 +0100

    fix: change html lang automatically

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -34,4 +34,10 @@ i18n.use(initReactI18next).init({
   },
 });
 
+i18n.on("languageChanged", (lng) => {
+  document.documentElement.lang = lng;
+});
+
+document.documentElement.lang = i18n.language;
+
 export default i18n;
