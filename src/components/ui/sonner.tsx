ecee3faf26406023bc24591c8595a8b0c504fe27
Mirko Lenz <mirko@mirkolenz.com>
2026-06-07 22:27:27 +0200

    chore(deps/shadcn): update

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,16 +1,21 @@
+"use client"
+
 import {
   CircleCheckIcon,
   InfoIcon,
   Loader2Icon,
   OctagonXIcon,
   TriangleAlertIcon,
-} from "lucide-react";
-import { Toaster as Sonner, type ToasterProps } from "sonner";
+} from "lucide-react"
+import { useTheme } from "next-themes"
+import { Toaster as Sonner, type ToasterProps } from "sonner"
 
-function Toaster({ ...props }: ToasterProps) {
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme()
+
   return (
     <Sonner
-      theme="system"
+      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
@@ -29,7 +34,7 @@ function Toaster({ ...props }: ToasterProps) {
       }
       {...props}
     />
-  );
+  )
 }
 
-export { Toaster };
+export { Toaster }
